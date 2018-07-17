b51987bf3095ce50e7aaad251894dd1ae98249e7
TianDaGe <tiandage719@outlook.com>
2018-07-17 08:26:15 +0000

    update blockexplorer ui color

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -421,7 +421,7 @@ std::string AddressToString(const CBitcoinAddress& Address)
     TxContent += "</table>";
 
     std::string Content;
-    Content += "<h1 style='color:#ffffff;'>" + _("Transactions to/from") + "&nbsp;<span>" + Address.ToString() + "</span></h1>";
+    Content += "<h1 style='color:#727070;'>" + _("Transactions to/from") + "&nbsp;<span>" + Address.ToString() + "</span></h1>";
     Content += TxContent;
     return Content;
 }
@@ -550,7 +550,7 @@ void BlockExplorer::setBlock(CBlockIndex* pBlock)
 
 void BlockExplorer::setContent(const std::string& Content)
 {
-    QString CSS = "body {font-size:12px; color:#f8f6f6; bgcolor:#fafafa;}\n a, span { font-family: monospace; }\n span.addr {color:#fafafa; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #fafafa;}\n td.d0 {font-weight: bold; color:#f8f6f6;}\n h2, h3 { white-space:nowrap; color:#fafafa;}\n a { color:#88f6f6; text-decoration:none; }\n a.nav {color:#fafafa;}\n";
+    QString CSS = "body {font-size:12px; color:#f8f6f6; bgcolor:#727070;}\n a, span { font-family: monospace; }\n span.addr {color:#727070; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #727070;}\n td.d0 {font-weight: bold; color:#f8f6f6;}\n h2, h3 { white-space:nowrap; color:#727070;}\n a { color:#88f6f6; text-decoration:none; }\n a.nav {color:#727070;}\n";
     QString FullContent = "<html><head><style type=\"text/css\">" + CSS + "</style></head>" + "<body>" + Content.c_str() + "</body></html>";
     // printf(FullContent.toUtf8());
 
